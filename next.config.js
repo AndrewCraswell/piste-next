216bcb365bcecd26e5ee5ce841255395cd8ed42d
Andrew Craswell <ajcraswell@gmail.com>
2022-01-09 18:24:34 -0800

    Add bundle analyzer to the plugins

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/graphql",
-        destination: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
-      },
-    ]
-  },
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE_BUNDLE === "true",
+})
+
+module.exports = withBundleAnalyzer(
+  /** @type {import('next').NextConfig} */
+  {
+    reactStrictMode: true,
+    trailingSlash: true,
+    async rewrites() {
+      return [
+        {
+          source: "/api/graphql",
+          destination: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
+        },
+      ]
+    },
+  }
+)
