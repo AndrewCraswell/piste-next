9ad48e9bfbfdad83955a6613c993bb5ae6f32493
Andrew Craswell <ajcraswell@gmail.com>
2022-05-11 17:26:30 -0700

    Add calendar poc

---
+++ src/hooks/useTabs.ts
@@ -0,0 +1,26 @@
+import {
+  TabValue,
+  SelectTabEvent,
+  SelectTabData,
+} from "@fluentui/react-components/dist/unstable"
+import { useState, useCallback, useMemo } from "react"
+
+export function useTabs(defaultTabName?: string) {
+  const [selectedTab, setTab] = useState<TabValue>(defaultTabName)
+
+  const onTabSelected = useCallback(
+    (event: SelectTabEvent, data: SelectTabData) => {
+      setTab(data.value)
+    },
+    []
+  )
+
+  return useMemo(
+    () => ({
+      selectedTab,
+      setTab,
+      onTabSelected,
+    }),
+    [onTabSelected, selectedTab]
+  )
+}
