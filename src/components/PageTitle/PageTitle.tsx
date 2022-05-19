ff2b118237b7b3d4696986e528706873b08ec558
Andrew Craswell <ajcraswell@gmail.com>
2022-05-19 00:51:46 -0700

    Convert page title to Fluent v9 Headline

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,12 +1,12 @@
 import styled from "@emotion/styled"
-import { Text } from "@fluentui/react-components"
+import { Headline } from "@fluentui/react-components"
 
-const Title = styled(Text)`
+const Title = styled(Headline)`
   margin: 0 0 1rem 0;
 `
 
 export const PageTitle: React.FunctionComponent = ({ children }) => (
-  <Title as="h1" size={500} weight="semibold" block>
+  <Title as="h1" block>
     {children}
   </Title>
 )
