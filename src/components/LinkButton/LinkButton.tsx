6d2d75e217ab31abd1333cc282a86aeb1a5d54cd
Andrew Craswell <ajcraswell@gmail.com>
2022-05-18 20:47:12 -0700

    Improve the LinkButton to use link shims

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,9 +1,32 @@
 import styled from "@emotion/styled"
-import { Button } from "@fluentui/react-components"
+import { Button, ButtonProps } from "@fluentui/react-components"
+import { useLinkShims } from "$hooks"
 
-export const LinkButton = styled(Button)`
+const UnstyledButton = styled(Button)`
   text-decoration: none;
 `
-LinkButton.defaultProps = {
+
+UnstyledButton.defaultProps = {
   as: "a",
+}
+
+type LinkButtonProps = ButtonProps & { href: string }
+
+export const LinkButton: React.FunctionComponent<LinkButtonProps> = (props) => {
+  const { children, ...buttonProps } = props
+  const { onClick, onMouseOver } = useLinkShims()
+
+  const href = (buttonProps as any)["href"] as string
+  return (
+    // @ts-ignore
+    <UnstyledButton
+      {...buttonProps}
+      as="a"
+      href={href}
+      onClick={onClick}
+      onMouseOver={onMouseOver}
+    >
+      {children}
+    </UnstyledButton>
+  )
 }
