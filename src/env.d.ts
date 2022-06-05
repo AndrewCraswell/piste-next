be7dec3942a3b1da7730268599c93ec00b5bc225
Andrew Craswell <ajcraswell@gmail.com>
2022-06-05 15:57:36 -0700

    Properly type environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,13 @@
 interface ImportMetaEnv {
   readonly VITE_BRANDING_NAME: string
   readonly VITE_SITE_NAME: string
+  readonly VITE_GOOGLE_PLACES_API_KEY: string
+  readonly VITE_AUTH0_CLIENT_ID: string
+  readonly VITE_AUTH0_DOMAIN: string
+  readonly VITE_GRAPHQL_API_URL: string
+  readonly VITE_AUTH0_HASURA_AUDIENCE: string
+  readonly VITE_NYLAS_CLIENT_ID: string
+  readonly VITE_NYLAS_API_URL: string
 }
 
 interface ImportMeta {
