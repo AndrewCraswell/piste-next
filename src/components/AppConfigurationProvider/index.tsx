12e7251f6ffda3e8b40f68f07922d517dadb1c51
Andrew Craswell <ajcraswell@gmail.com>
2022-09-22 01:44:36 -0700

    Track user logged in against Launch Darkly

--- src/components/AppConfigurationProvider/index.tsx
+++ src/components/AppConfigurationProvider/index.tsx
@@ -1,4 +1,5 @@
-import { LDProvider } from "launchdarkly-react-client-sdk"
+import { useAuth0 } from "@auth0/auth0-react"
+import { LDProvider, useFlags } from "launchdarkly-react-client-sdk"
 
 export interface IAppConfigurationProviderProps {
   code: string
@@ -7,10 +8,22 @@ export interface IAppConfigurationProviderProps {
 export const AppConfigurationProvider: React.FunctionComponent<
   IAppConfigurationProviderProps
 > = ({ code, children }) => {
+  const { user } = useAuth0()
+  const { email, sub: userId } = user || {}
+
   return (
     <LDProvider
+      user={{
+        key: userId,
+        email,
+      }}
       clientSideID={code}
-      options={{ bootstrap: "localStorage", streaming: true }}
+      options={{
+        bootstrap: "localStorage",
+        streaming: true,
+        useReport: true,
+      }}
+      deferInitialization
     >
       {children}
     </LDProvider>
