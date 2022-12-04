d7d3525fb88c33f643539cf21e78f2c8c8c77d9c
Andrew Craswell <ajcraswell@gmail.com>
2022-12-04 01:22:48 -0800

    Improve member details list component

--- src/components/Cards/MemberDetailsCard/MemberDetailsCard.tsx
+++ src/components/Cards/MemberDetailsCard/MemberDetailsCard.tsx
@@ -22,10 +22,9 @@ const MemberDetailsList = styled.ul`
   }
 `
 
-const MemberDetailItem: React.FunctionComponent<IMemberDetailItemProps> = ({
-  title,
-  value,
-}) => (
+export const MemberDetailItem: React.FunctionComponent<
+  IMemberDetailItemProps
+> = ({ title, value }) => (
   <li>
     <Text size={100} weight="semibold">
       {title}
@@ -45,7 +44,7 @@ const MemberPersona = styled(PersonaAvatar)`
 
 export interface IMemberDetailItemProps {
   title: string
-  value?: string
+  value?: string | number | null
 }
 
 export type MemberDetails = {
