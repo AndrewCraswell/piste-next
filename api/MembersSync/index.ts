eacc429848df70bb642e0ebc9f77d73b97d54fb3
Andrew Craswell <ajcraswell@gmail.com>
2023-12-28 22:33:35 -0800

    Improve member sync function

--- api/MembersSync/index.ts
+++ api/MembersSync/index.ts
@@ -66,44 +66,59 @@ const scheduledIngest: AzureFunction = async function (
   context: Context,
   timer: any
 ): Promise<void> {
+  context.log(
+    `Fetching the USA Fencing members page... [${process.env.USA_FENCING_MEMBERS_CSV_URL}]`
+  )
+
   // Request the members page
-  const { data: response } = await axios.get<string>(
-    process.env.USA_FENCING_MEMBERS_CSV_URL
-  )
+  try {
+    const { data: response } = await axios.get<string>(
+      process.env.USA_FENCING_MEMBERS_CSV_URL
+    )
 
-  // Parse the URL to the latest document
-  const match = response.match(
-    /<a href="(.*)" class="card p-2 text-white bg-blued tight d-inline-block no-link">/
-  )
-  let membersCsvUrl = ""
-  if (match.length === 2) {
-    membersCsvUrl = he.decode(match[1])
-  } else {
-    // TODO: Warn that the member file wasn't parsed
+    context.log("Fetched the USA Fencing members page.")
+
+    // Parse the URL to the latest document
+    const match = response.match(
+      /<a href="(.*)" class="card p-2 text-white bg-blued tight d-inline-block no-link">/
+    )
+    let membersCsvUrl = ""
+    if (match.length === 2) {
+      membersCsvUrl = he.decode(match[1])
+      context.log(membersCsvUrl)
+    } else {
+      context.log("Could not locate download link for the members CSV.")
+      // TODO: Send an email alert that the file was not parsed correctly
+    }
+
+    // Fetch the CSV file
+    try {
+      const members: Array<IAssociationMember> = []
+      await csv()
+        .fromStream(request.get(membersCsvUrl))
+        .subscribe((row: CsvMemberRow) => {
+          return new Promise((resolve, reject) => {
+            const member = parseMember(row)
+
+            if (member.AssociationMemberId) {
+              members.push(member)
+            }
+
+            resolve()
+          })
+        })
+
+      context.log("Found", members.length, "members to ingest.")
+
+      // Upsert the results into the database
+      await updateDatabase(context, members)
+    } catch (e) {
+      // TODO: Send an email alert that the file was not parsed correctly
+      context.log("Could not parse the members CSV file.")
+    }
+  } catch (e) {
+    context.log("Could not fetch the USA Fencing members page.")
   }
-
-  context.log(membersCsvUrl)
-
-  // Fetch the CSV file
-  const members: Array<IAssociationMember> = []
-  await csv()
-    .fromStream(request.get(membersCsvUrl))
-    .subscribe((row: CsvMemberRow) => {
-      return new Promise((resolve, reject) => {
-        const member = parseMember(row)
-
-        if (member.AssociationMemberId) {
-          members.push(member)
-        }
-
-        resolve()
-      })
-    })
-
-  context.log("Found", members.length, "members to ingest.")
-
-  // Upsert the results into the database
-  await updateDatabase(context, members)
 }
 
 async function updateDatabase(
@@ -179,13 +194,13 @@ function getUpsertQuery(members: IAssociationMember[]) {
     query = `
       ${query}
 
-      UPDATE dbo.AssociationMembers WITH (UPDLOCK, SERIALIZABLE) SET 
+      UPDATE dbo.AssociationMembers WITH (UPDLOCK, SERIALIZABLE) SET
         ${columns
           .map((col) => `[${col}] = ${getSqlValue(member[col])},`)
           .join("")
           .slice(0, -1)}
       WHERE AssociationMemberId = '${member.AssociationMemberId}';
-      
+
       IF @@ROWCOUNT = 0
       BEGIN
         INSERT dbo.AssociationMembers(
